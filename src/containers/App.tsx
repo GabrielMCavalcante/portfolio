a4d540b93d030301227a7a0ce66dcd99f4338a48
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-06 15:11:00 -0300

    Moved App to containers folder

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import './App.css'
+
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      {/* 
-        <Header />
-        <Title />
+
+      <Header />
+      {/* <Title />
         <Skills />
         <Projects />
         <Contact />
