cc7ce5a52ca696d507c146721844251d2c3f7e89
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-06 17:45:12 -0300

    Created Title container component

---
+++ src/containers/Title/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// CSS styles
+import './styles.css'
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>Gabriel Cavalcante</h1>
+            <h3>Front-end developer</h3>
+        </div>
+    )
+}
+
+export default Title
