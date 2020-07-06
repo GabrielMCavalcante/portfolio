50b22ed84e7a38647ca6ad0129090509eea7c55d
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-06 11:54:24 -0300

    Added initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      
+      {/* 
+        <Header />
+        <Title />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      */}
     </div>
   )
 }
