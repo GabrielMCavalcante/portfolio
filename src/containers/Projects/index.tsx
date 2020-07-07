01a971559a6666560cbdab9d9014b7b1efd1af6c
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-07 18:42:17 -0300

    Added projects info objects

--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -3,17 +3,44 @@ import React from 'react'
 // Components
 import ProjectCards from 'components/ProjectCards'
 
+// Images
+import notes from 'assets/notes-project.png'
+import notesMinified from 'assets/notes-project-minified.png'
+import tictactoe from 'assets/tictactoe-project.png'
+import tictactoeMinified from 'assets/tictactoe-project-minified.png'
+import statisticsAnswerChecker from 'assets/statistics-answer-checker-project.jpg'
+import statisticsAnswerCheckerMinified from 'assets/statistics-answer-checker-project-minified.jpg'
+
 // CSS styles
 import './styles.css'
 
 const projects = [
     { 
-        imageURL: 'https://picsum.photos/300', 
+        imageURL: notes,
+        minifiedImageURL: notesMinified, 
         title: 'Notes', 
-        description: 'Create and edit personal annotations', 
+        description: 'A Vue - Firebase app in which the user can create, edit and delete personal annotations', 
         technologies: ['Vue', 'Vuex', 'Vuetify'],
         demoLink: 'https://notes-5cf6a.firebaseapp.com',
         sourceLink: 'https://github.com/GabrielMCavalcante/notes-firebase-app'
+    },
+    { 
+        imageURL: tictactoe,
+        minifiedImageURL: tictactoeMinified, 
+        title: 'Tic Tac Toe', 
+        description: 'A React project where you can play the famous game Tic Tac Toe', 
+        technologies: ['React', 'Redux', 'Typescript'],
+        demoLink: 'https://gabrielmcavalcante.github.io/TicTacToe/#/',
+        sourceLink: 'https://github.com/GabrielMCavalcante/TicTacToe'
+    },
+    { 
+        imageURL: statisticsAnswerChecker,
+        minifiedImageURL: statisticsAnswerCheckerMinified, 
+        title: 'Statistics Answer Checker', 
+        description: 'A React app that helps students check statistics exercises answers', 
+        technologies: ['React', 'Router', 'Typescript', 'Papaparse'],
+        demoLink: 'https://gabrielmcavalcante.github.io/statistics-answer-checker/#/',
+        sourceLink: 'https://github.com/GabrielMCavalcante/statistics-answer-checker'
     }
 ]
 
