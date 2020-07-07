202b3d62338b5ccbee672bd3493194b432156606
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-07 13:06:13 -0300

    Added Skills container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,16 +3,17 @@ import React from 'react'
 // Containers
 import Header from './Header'
 import Title from './Title'
+import Skills from './Skills'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Title />
-      {/*<Skills />
-        <Projects />
-        <Contact />
-        <Footer />*/}
+      <Skills />
+   {/*<Projects />
+      <Contact />
+      <Footer />*/}
     </div>
   )
 }
