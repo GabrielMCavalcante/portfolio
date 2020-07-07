3bca83c01f5e73f9cfd46f34a89eb3dcb261c59e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-07 13:06:31 -0300

    Created Skills container component

---
+++ src/containers/Skills/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// Components
+import Table from 'components/Table'
+
+// CSS styles
+import './styles.css'
+
+const skills = [
+    { header: 'Web Technologies', items: ['HTML 5', 'CSS 3', 'Typescript'] },
+    { header: 'Frameworks', items: ['React', 'React Native'] },
+    { header: 'State Management', items: ['Redux', 'Context API', 'React Hooks'] }
+]
+
+function Skills() {
+    return (
+        <div className="Skills">
+            <h2>My Skills</h2>
+            <Table data={skills}/>
+        </div>
+    )
+}
+
+export default Skills
