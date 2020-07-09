49e09d3a5237badd12fc2617e955c0e02a59ef6e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-09 09:43:37 -0300

    Refactored to fetch async skills

--- src/containers/Skills/index.tsx
+++ src/containers/Skills/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// Axios instance
+import axios from 'axios-config'
 
 // Components
 import Table from 'components/Table'
@@ -6,17 +9,42 @@ import Table from 'components/Table'
 // CSS styles
 import './styles.css'
 
-const skills = [
-    { header: 'Web Technologies', items: ['HTML 5', 'CSS 3', 'Typescript'] },
-    { header: 'Frameworks', items: ['React', 'React Native', 'Vue'] },
-    { header: 'State Management', items: ['Redux', 'Context API', 'React Hooks', 'Vuex'] }
-]
+interface Document {
+    fields: {
+        header: { stringValue: string },
+        items: { arrayValue: { values: { stringValue: string }[] } }
+    }
+}
+
+interface Skill {
+    header: string,
+    items: string[]
+}
 
 function Skills() {
+
+    const [skills, setSkills] = useState<Skill[]>()
+
+    useEffect(() => {
+        async function fetchProjects() {
+            const response = await axios.get('/skills')
+            const documents: Document[] = response.data.documents
+            const parsedSkills: Skill[] = []
+            documents.forEach(doc => {
+                parsedSkills.push({
+                    header: doc.fields.header.stringValue,
+                    items: doc.fields.items.arrayValue.values.map(value => value.stringValue)
+                })
+            })
+            setSkills(parsedSkills)
+        }
+        fetchProjects()
+    }, [])
+
     return (
         <div className="Skills">
             <h2>My Skills</h2>
-            <Table data={skills}/>
+            {skills && <Table data={skills} />}
         </div>
     )
 }
