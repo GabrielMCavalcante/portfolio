7f2243438a5c42973d422e2c0b1da3642af481be
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-10 17:54:45 -0300

    Added copy email function

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Icons
-import { FiGithub, FiLinkedin, FiMail } from 'react-icons/fi'
+import { FiGithub, FiLinkedin, FiMail, FiLink2 } from 'react-icons/fi'
 import { FaCopyright } from 'react-icons/fa'
 
 // CSS styles
 import './styles.css'
 
 function Footer() {
+
+    const [copied, setCopied] = useState(false)
+
+    function copyMail() {
+        setCopied(false)
+        const input = document.createElement('input')
+        input.value = 'contato.gabrielmcavalcante@gmail.com'
+        document.body.appendChild(input)
+        input.select()
+        document.execCommand("Copy")
+        document.body.removeChild(input)
+        setCopied(true)
+        setTimeout(() => setCopied(false), 3000)
+    }
+
+    const classes = ["CopyFeedback"]
+    if(copied) classes.push("Show")
+
     return (
         <footer className="Footer">
             <p><FaCopyright /> <span>Gabriel Cavalcante - 2020</span></p>
@@ -26,7 +44,12 @@ function Footer() {
                 <FiLinkedin />
                 <span>LinkedIn</span>
             </a></p>
-            <p><FiMail /><span>contato.gabrielmcavalcante@gmail.com</span></p>
+            <p className="Email">
+                <FiMail />
+                <span>contato.gabrielmcavalcante@gmail.com</span>
+                <span className="CopyMail" onClick={copyMail}><FiLink2 /></span>
+                <span className={classes.join(' ')}>Copied!</span>
+            </p>
         </footer>
     )
 }
