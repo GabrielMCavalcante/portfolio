34cb9a957c7270e941113c95cc19a055155e7552
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-09 22:11:18 -0300

    Created FormResponse component

---
+++ src/components/UI/FormResponse/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+// Icons
+import { FiCheckCircle, FiXCircle, FiX } from 'react-icons/fi'
+
+// CSS styles
+import './styles.css'
+
+function FormResponse(props: { status: string, onclose: () => void}) {
+    return (
+        <div className="FormResponse">
+            <div className="FormResponseContent">
+                <div className="CloseIcon" onClick={props.onclose}><FiX /></div>
+                <div className="Response">
+                    <h2>{
+                        props.status === 'Ok'
+                        ? 'Email sent successfully!'
+                        : 'An error occurred whilst sending the email. Try Again.'
+                    }</h2>
+                    <div className="StatusIcon">
+                        {
+                            props.status === 'Ok' 
+                            ? <FiCheckCircle />
+                            : <FiXCircle />
+                        }
+                    </div>
+                    <button onClick={props.onclose}>Close</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormResponse
