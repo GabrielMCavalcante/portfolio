4f526605b793fa6acace2de7cf75f01799387ab8
GabrielMCavalcante <56729382+GabrielMCavalcante@users.noreply.github.com>
2020-07-11 14:07:36 -0300

    Update init.ts

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBGAE1WQYGWxg_ozzK0hfYNrsDH35TrXXo",
-    authDomain: "gabrielcavalcante-portfolio.firebaseapp.com",
-    databaseURL: "https://gabrielcavalcante-portfolio.firebaseio.com",
-    projectId: "gabrielcavalcante-portfolio",
-    storageBucket: "gabrielcavalcante-portfolio.appspot.com",
-    messagingSenderId: "392227781922",
-    appId: "1:392227781922:web:c185728e6af21d959f397b"
+    // generated firebase configurations
 }
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
